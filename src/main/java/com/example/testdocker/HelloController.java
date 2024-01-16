cd010bb8cbc8b90692cbea67f33ed6e1d6400011
오은아 <erropp90@naver.com>
2024-01-16 16:01:29 +0900

    Feat: Docker test

---
+++ src/main/java/com/example/testdocker/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.testdocker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getMsg(){
+        return "Hello World";
+    }
+
+}
